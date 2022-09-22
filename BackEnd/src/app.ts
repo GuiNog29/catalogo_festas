886c047ab20e8050e50c4584358546c392567826
Guilherme Almeida <guimathiasgui@gmail.com>
2022-09-21 21:23:13 -0300

    Creating a test route

--- BackEnd/src/app.ts
+++ BackEnd/src/app.ts
@@ -2,8 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import logger from 'morgan';
 
+import { router } from './Routes/index';
+
 export const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(logger('dev'));
+
+app.use('/', router);
